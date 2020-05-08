d48bb4d19b2791eb0538823f53dfd67d32281146
Saida-Song <seedsong25@gmail.com>
2020-05-08 14:10:10 -0700

    favicon added

--- dev/app.js
+++ dev/app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const favicon = require("serve-favicon");
+const path = require("path");
 const app = express();
 const port = 8080;
 
@@ -29,5 +31,7 @@ app.get("/signUp", (req, res) => {
 app.get("/aboutUs", (req, res) => {
   res.render("pages/aboutUs");
 });
+
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 app.listen(port);
