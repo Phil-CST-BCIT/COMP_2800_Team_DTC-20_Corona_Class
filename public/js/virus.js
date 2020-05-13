dda210eb6292a7ebe25347d3885924e070b14483
Saida-Song <seedsong25@gmail.com>
2020-05-13 14:13:00 -0700

    game modified

--- public/js/virus.js
+++ public/js/virus.js
@@ -1,25 +1,36 @@
+// Counter
 let score = 0;
 let id = 0;
+// Global variable being used to assign to the setInterval()
 let interval;
+
+// Assign the variable inside a function to prevent it's been triggered immediately
 let startGame = () => {
   interval = setInterval(setImage, 3000);
 };
+
+//Trigger the setInterval() after 10 sec
 $(document).ready(function () {
   setTimeout(startGame, 10000);
 });
+
 function setImage() {
   let img = $(
     `<img src='/css/images/virus.png' id='${++id}' class='virus' style='left:${randomLeft()}; top:${randomTop()}; position:absolute; width:75px;' />`
   );
 
+  // animation
   img
     .appendTo("body")
     .slideUp(0)
     .fadeIn(1000)
+
+    //Click on the image will remove it
     .on("click", function () {
       score++;
-      console.log($(this).attr("id"), score);
       $(this).remove();
+
+      // Stopping interval and remove all the virus images
       if (score > 4) {
         $(".virus").remove();
         clearInterval(interval);
