9a3e28707c8c0f78c4843c4fa90b28c0685d2fe4
Kyle Bulalakaw <kbulalakaw2@my.bcit.ca>
2020-05-14 12:40:18 -0700

    changed function in quiz.js to properly replace content of quiz.ejs, added data and get statement for quiz questions in app.js

--- app.js
+++ app.js
@@ -32,8 +32,50 @@ app.get("/quizStart", (req, res) => {
   res.render("pages/quizStart");
 });
 
+// for quiz page
+let qPool = [
+  {
+      id: 1,
+      question: 'Which is not a transmission method of the Coronavirus?',
+      answer: 'Cellular network signals',
+      options: ['Direct contact with infected persons', 'Contact with contaminated surfaces', 'Cellular network signals', 'Contact with infected respiratory droplets, as exhaled by an infected person']
+  },
+  {
+      id: 2,
+      question: 'When are non-medical masks most effective at slowing the spread of COVID-19?',
+      answer: 'When used by infected persons around uninfected',
+      options: ['When used by uninfected persons in public places', 'When used by uninfected persons around infected', 'Masks are ineffective at slowing the spread', 'When used by infected persons around uninfected']
+  },
+  {
+      id: 3,
+      question: 'Which is NOT a symptom of the Coronavirus?',
+      answer: 'Numbness in limbs',
+      options: ['Fever', 'Fatigue', 'Headaches', 'Numbness in limbs']
+  }
+];
+
+let firstQuestion = {
+  question: 'Which is not a transmission method of the Coronavirus?',
+  answer: 'Cellular network signals',
+  options: ['Direct contact with infected persons', 'Contact with contaminated surfaces', 'Cellular network signals', 'Contact with infected respiratory droplets, as exhaled by an infected person']
+};
 app.get("/quiz", (req, res) => {
-  res.render("pages/quiz");
+  quizIndex = 0;
+  res.render("pages/quiz", firstQuestion);
+});
+
+// from 2537 week 3 package
+let quizIndex = 0;
+app.get('/nextQuestion', (req,res) => {
+
+  ++quizIndex; 
+
+  if (quizIndex >= qPool.length) {
+      res.render("pages/quizScore");
+  }
+  else {
+    res.json(qPool[quizIndex]);
+  }
 });
 
 app.get("/quizScore", (req, res) => {
--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,9 +1,43 @@
-// quiz score and nav logic
-
+// quiz testing + score and nav logic
 let qPool = [
-    {question: '',
-        answer: '',
-        options: ['', '', '', '']}
-
+    { // question 1
+        question: 'Which is not a transmission method of the Coronavirus?',
+        answer: 'Cellular network signals',
+        options: ['Direct contact with infected persons', 'Contact with contaminated surfaces', 'Cellular network signals', 'Contact with infected respiratory droplets, as exhaled by an infected person']
+    },
+    { // question 2
+        question: 'When are non-medical masks most effective at slowing the spread of COVID-19?',
+        answer: 'When used by infected persons around uninfected',
+        options: ['When used by uninfected persons in public places', 'When used by uninfected persons around infected', 'Masks are ineffective at slowing the spread', 'When used by infected persons around uninfected']
+    },
+    { // question 3
+        question: 'Which is NOT a symptom of the Coronavirus?',
+        answer: 'Numbness in limbs',
+        options: ['Fever', 'Fatigue', 'Headaches', 'Numbness in limbs']
+    }
 ];
+
+// from 2537 week 3 starter code package
+$(document).ready(function(){
+
+    $('.option-item').on('click', function() {
+        
+        $.ajax('/nextQuestion')
+            .done(function(data) {
+                let options = data.options;
+                console.log(data);
+                $('#question-number').text(data.id);
+                $('#question').text(data.question);
+                $('#option-a').text(options[0]);
+                $('#option-a').text(options[1]);
+                $('#option-b').text(options[2]);
+                $('#option-c').text(options[3])
+                
+            })
+            .fail(function(error){
+                console.log(error);
+            })
+
+    })
+})
 
